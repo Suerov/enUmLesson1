a6a4c732564c98414bcada0afb94a9376e8b0403
Suerov <suerovm2@gmail.com>
2023-07-04 18:06:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        StringBuilder str = new StringBuilder();
+        for (int a:arr) {
+            str.append(a);
+
+        }
+        System.out.println(str);
+
+
+    }
+
+    }
